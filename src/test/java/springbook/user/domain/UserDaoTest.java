679b585c2f399fde663cbaf4b5f5d7b787eea61e
cheadev <cheadev@naver.com>
2022-02-18 00:47:24 +0900

    1.1 초난감 DAO p54~59

---
+++ src/test/java/springbook/user/domain/UserDaoTest.java
@@ -0,0 +1,27 @@
+package springbook.user.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+
+    @Test
+    void addAndGet() throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("id");
+        user.setName("name");
+        user.setPassword("pwd");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록성공");
+
+        User get = dao.get(user.getId());
+        System.out.println(get.getId() + " 조회성공");
+    }
+}
