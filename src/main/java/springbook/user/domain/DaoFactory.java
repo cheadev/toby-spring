03ad6e556829c5bc940ddb038c45ac25a861fdd4
cheadev <cheadev@naver.com>
2022-02-18 17:21:45 +0900

    1.8  XML을 이용하는 애플리케이션 컨텍스트 126~135

--- src/main/java/springbook/user/domain/DaoFactory.java
+++ src/main/java/springbook/user/domain/DaoFactory.java
@@ -1,13 +1,19 @@
 package springbook.user.domain;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
-        UserDao userDao = new UserDao(connectionMaker());
+        UserDao userDao = new UserDao();
+        userDao.setConnectionMaker(connectionMaker());
         return userDao;
     }
 
+    @Bean
     public ConnectionMaker connectionMaker() {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        return connectionMaker;
+        return new DConnectionMaker();
     }
 }
--- src/test/java/springbook/user/domain/UserDaoTest.java
+++ src/test/java/springbook/user/domain/UserDaoTest.java
@@ -1,6 +1,9 @@
 package springbook.user.domain;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
@@ -10,7 +13,8 @@ class UserDaoTest {
 
     @Test
     void addAndGet() throws SQLException, ClassNotFoundException {
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
         user.setId("id");
